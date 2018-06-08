c01fc88c44ca40555946fcb8e45e751dec994685
youngjun <showme0077@naver.com>
2018-06-08 17:24:22 +0900

    망했어

---
+++ keyboard.1.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+    echo '
+    {    
+        "type": "buttons",
+        "buttons": ["메뉴1", "메뉴2", "메뉴3"]
+    }';
+?>
---
+++ message.php
@@ -0,0 +1,69 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+$data = json_decode(file_get_contents('php://input'), true);
+$content = $data["content"];
+
+// $poolText = preg_grep("바보", $pool);
+
+// $fl_array = preg_grep("/^(\d+)?\.\d+$/", $array);
+
+
+
+switch($content)
+{
+    case "/html ?":
+    case "?":
+    case "help":
+        echo '
+            {
+                "message":
+                {
+                    "text": "명령어 목록\\n1. html\\n2. head\\n3. body\\n숫자 혹은 /html 명령어 형식으로 검색해주세요."
+                }
+            }';
+    break;
+
+    case "/html html":
+    case 1:
+        echo '
+            {
+                "message":
+                {
+                    "text": "html은 하이퍼텍스트 마크업 언어(HyperText Markup Language)이며,\\n웹페이지를 제작하기 위한 언어이다.\\nhtml 태그가 있어야 브라우저가 해당 파일이 html인 것을 확인하고, 태그 안에 있는 내용을 실행시킨다."
+                }
+            }';
+    break;
+
+    case "/html head":
+    case 2:
+        echo '
+            {
+                "message":
+                {
+                    "text": "head 태그는 페이지에서는 보이지 않는 정보인 메타 데이터를 모아둔 태그이다.\\n또, js와 css 파일의 파일을 연결한다."
+                }
+            }';
+    break;
+
+    case "/html body":
+    case 3:
+        echo '
+            {
+                "message":
+                {
+                    "text": "body 태그는 실질적으로 웹페이지의 내용을 작성한 태그이다.\\nhead 태그가 보이지 않는 데이터라면, body는 눈에 보이는 데이터다."
+                }
+            }';
+    break;
+
+    default:
+        echo '
+            {
+                "message":
+                {
+                    "text": "잘못된 명령어입니다."
+                }
+            }';
+    break;
+}
+?>
